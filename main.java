cb9509c5a77448662be22e34950e9cb3c0308382
Karthikeyan <160267134+psyclox@users.noreply.github.com>
2024-02-25 13:18:26 +0000

    Update main.java

--- main.java
+++ main.java
@@ -1,27 +1,24 @@
 import java.security.SecureRandom;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
     public class Main {
     public static void main(String[] cool_buddy) {
-        Scanner input=new Scanner(System.in);
-        System.out.print("Enter the Password length: ");
-        try {
-            int length=input.nextInt();
-            if (length<=0) {
-                System.out.println("Password length should be greater than 0.");
-                return;
-            }
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter the Password length: ");
+    try {
+    int length=input.nextInt();
+    if (length<=0) {
+    System.out.println("Password length should be greater than 0.");
+        return;
+                    }
             String upCase="QWERTYUIOPLKJHGFDSAZXCVBNM";
             String lowCase="qwertyuioplkjhgfdsazxcvbnm";
             String spechar="!@#$%^&*()_+-=[]{}.>|?/<";    
             StringBuilder passkoder=new StringBuilder();
-
             SecureRandom gen=new SecureRandom();
-
             for (int i=0;i<length;i++) {
-                int wand=gen.nextInt(4);
-                switch (wand) 
+        int wand=gen.nextInt(4);
+        switch (wand) 
                 {
                     case 1:
                         passkoder.append(gen.nextInt(10)); 
